9acc7e61d34a1a6c01c4f8f7bb6987b2793cf1e9
DW-203 <DW-203@DESKTOP-C0H6A9G>
2026-01-05 17:28:05 +0900

    1월 5일 과제 및 수업

---
+++ java_control/src/Study20260105/LoopMain.java
@@ -0,0 +1,213 @@
+package Study20260105;
+
+import java.util.Scanner;
+
+public class LoopMain {
+
+	public static void main(String[] args) {
+
+//		for(int i = 1; i<=10; i++) {
+//			System.out.println( i );
+//		}
+//		for (int i =1; i <=100; i++) {
+//			if(i %10 == 0 );
+//			System.out.println( i );
+//		}
+
+		Scanner scan = new Scanner(System.in);
+
+//		System.out.println("정수를 입력 :");
+//		int num = scan.nextInt();
+//		
+//		for(int i=1; i<=num; i++) {
+//			System.out.println( i );
+//		}
+
+		// 1부터 12까지 출력하는데
+		// 4의 배수에는 four 라고 출력하시오
+
+//		for (int i=1; i<=12; i++) {
+//			if (i%4==0) {
+//				System.out.println("four");
+//			}else {
+//				System.out.println( i );
+//			}
+//			
+//		
+//			
+//		}
+//		int count=0;
+//		
+//		for(int i=1; i<=1000; i++) {
+//			
+//			if(i%17==0) {
+//				count++;	
+//			}
+//		}
+//		System.out.println("17의 배수 :"+count+"개");
+
+		// 문제
+		// 동전 앞면 뒷면 맞추기 게임
+		// 게임을 총 10판 진행 하기
+		// 게임에서 내가 맟춘 횟수는 총몇번인진 출력하기
+
+//		System.out.println("앞면 1,  뒷면 2,");
+//
+//		int count = 0;
+//
+//		for (int i = 1; i <= 10; i++) {
+//			int coin = (int) Math.floor(Math.random() * 2) + 1;
+//
+//			System.out.print(i+".입력 :");
+//			int num = scan.nextInt();
+//
+//			if (coin == num) {
+//				count++;
+//				System.out.println("성공!!");
+//			} else {
+//				System.out.println("실패");
+//			}
+//		}
+//		System.out.println("맞춘횟수 : " + count);
+
+		
+		//무한루프 - 반복문은 조건식이 참인경우 계속 반복수행을 한다.
+		//         무조건 참이 되게 조건식을 설정하면 무한반복을 할수있다.
+		// 무한루프는 지정된 횟수가 없기에 멈추지 않고 계속 동작한다.
+		// 그래서 무한루프 구현시 특정조건을 걸어 둔다.
+		
+//		for (;true;) {
+//			
+//		}
+//		
+//		for(;;) {
+//			;
+//		}
+		
+//		while(true) {
+//			System.out.println("나는 무한 루프이다. ");
+//		}
+		
+//		int i = 10;
+//		while (true) {
+//			System.out.println(i);
+//			i++;
+//			if (i==40)break;
+//		}
+		
+//		int count = 0; 
+//		
+//		while(true) {
+//			int num = (int)Math.floor(Math.random()*100)+1;
+//			count++;
+//			System.out.println(count+" 회 "+num);
+//			if(num == 11 ) break;
+//		}
+		
+		
+//		int com = (int)Math.floor(Math.random()*50)+1;
+//		
+//		while(true) {
+//			System.out.print("1~50 중 입력 :");
+//			int user = scan.nextInt();
+//			
+//			if( user > com ) {
+//				System.out.println("Down");
+//			}
+//			if( user < com ) {
+//				System.out.println("up");
+//			}
+//			if(user == com) {
+//				System.out.println("정답");
+//			}
+//		}
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		// 과제
+//		 가위 바위 보 게임을 총 15 판 진행
+//		 *  게임을 진행 하면서 1판 끝날때마다
+//		 *  몇승, 몇패 , 몇무 출력하기
+		
+		// 1.가위  2.바위  3.보
+		
+		int count = 0;
+		
+		
+		for (int i=1; i<=15; i++) {
+			int user = (int)Math.floor(Math.random()*3)+1;
+			int com = (int)Math.floor(Math.random()*3)+1;
+			
+			if (user - com == ) 
+			
+		}
+		 
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+	}
+
+}
+
+/*
+ *  1월 5일 과제
+ *  
+ *  가위 바위 보 게임을 총 15 판 진행
+ *  게임을 진행 하면서 1판 끝날때마다
+ *  몇승, 몇패 , 몇무 출력하기
+ *  
+ *  추과 과제
+ *  
+ *  내가 숫자 하나를 제시하고 컴퓨터가 그 숫자를 맞출수있도록 내가 범위를 점점 줄여가며
+ *  컴퓨터가 맞출수있도록 내가 랜덤 범위를 계속 줄일수있는 코딩 짜기
+ * 
+ * 
+ * 
+ * 
+ * 반복문 - for , while , do ~ while 반복문이란 특정코드를 정해진 횠수만큼 동작 시키는 과정이다. 또는 횟수의 제한
+ * 없이는 동작시킬수없다
+ * 
+ * for 문
+ * 
+ * for ( 초기값; 조건식; 증감식 ) { 반복실행할 내용; 반복실행 할 내용; }
+ * 
+ * for (int i=3; i<=9; i++){
+ * 
+ * }
+ * 
+ * 1부터 시작하여 1씩 증가하는 형태로 반복문을 작성한다. 그래야 몇번 반복 시킬것인지 작성하기도 편하고 파악하기도 빠르고
+ * 
+ * 
+ * for (Member m : MamberList){
+ * 
+ * } 자주 사용하게될 for 문의 형태
+ * 
+ */
+
+
+
+
+
+
+
--- java_control/src/Study20260105/MainMethod.java
+++ java_control/src/Study20260105/MainMethod.java
@@ -1,9 +1,93 @@
 package Study20260105;
 
+import java.util.Scanner;
+
 public class MainMethod {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		
+		//Integer.parseInt() 정수갑으로 변경
+		//Math.floor() 소수점 자르기
+		
+//		int num = (int)Math.floor(Math.random()*10);
+//		
+//		System.out.println(num);
+		
+		//동전의 앞면 뒷면 맞추기
+		Scanner sc = new Scanner(System.in);
+		
+//		int coin = (int)Math.floor(Math.random()*2)+1;
+//		// 1 - 앞면 , 2 - 뒷면
+//		System.out.println("동전 앞면 뒷면 맟추기");
+//		System.out.println("1 - 앞면, 2 - 뒷면 \n 입력 :");
+//		int user = sc.nextInt();
+//		
+//		if (user == coin)
+//		System.out.println("맞추기 성공!!!");
+//		else
+//			System.out.println("맞추기 실패!!!");
+		
+//		int dice = (int)Math.floor(Math.random()*6)+1;
+//		
+//		System.out.println("주사위 맞추기 게임!!!");
+//		System.out.println("1~6중에 하나 입력 :");
+//	    int num = sc.nextInt();
+//	    
+//		if (num == dice)
+//	    System.out.println("맞추기 성공");
+//		else
+//			System.out.println("맟추기 실패");
+		
+		//컴퓨터 주사위
+//		int com = (int)Math.floor(Math.random()*6)+1;
+//		
+//		//나의 주사위
+//		int user = (int)Math.floor(Math.random()*6)+1;
+//		// 내가 컴퓨터 보다 큰가? 작은가? 아니면 비김?
+//		
+//		
+//		if (user > com || (user==1 && com==6)) {
+//			if ( user == 6 && com==1)
+//				System.out.println("나의 패배");
+//			else
+//			System.out.println("나의 승");
+//		}else if (user == com)
+//			System.out.println("비김");
+//		else
+//			System.out.println("나의 패배");
+		
+		// 컴퓨터와 함께하는 가위 바위 보 게임!!!
+		// 1 - 가위 , 2 - 바위 , 3 - 보
+		
+		//컴퓨터의 가위바위보 값은 키보드 입력으로
+		//내가 이겼는지 , 졌는지 , 비겼는지 출력하시오
+		
+		int user = (int)Math.floor(Math.random()*3)+1;
+		int com = (int)Math.floor(Math.random()*3)+1;
+		
+		System.out.println("사용자 :"+user);
+		System.out.println("컴퓨터 :"+com);
+		
+//		if (user > com || (user == 1 && com == 3)) { 
+//			if (user == 3 && com ==1)
+//			System.out.println("패배");
+//			else
+//			System.out.println("승리");	
+//		}else if (user < com)
+//			System.out.println("패배");
+//		else if (user == com)
+//			System.out.println("무승부");
+		
+		int res = user - com;
+		if (res == 0)
+			System.out.println("무승부");
+		else if (res == 2 || res == 1)
+			System.out.println("승리");
+		else 
+			System.out.println("패배");
+		
+		
+		
 
 	}
 
