5a6233bd857ce160316398a6d7f196b08666761e
DW-203 <DW-203@DESKTOP-C0H6A9G>
2026-01-06 12:50:32 +0900

    20260106

---
+++ java_control/src/Study20260106/ArrayMain.java
@@ -0,0 +1,22 @@
+package Study20260106;
+
+public class ArrayMain {
+
+	public static void main(String[] args) {
+		
+		int[] arr = new int[50];
+		//arr 은 참조변수이다, 참조변수는 메모리 주소를 저장할수있는변수이다.
+		System.out.println();
+		int []arr2 = {10,20,30};
+	}
+
+}
+/*
+ *  배열 - 동일한 데이터타입의 데이터 저장공간이 연속적으로 나열되어 있는구조
+ * 
+ * 
+ * 
+ */
+
+
+
